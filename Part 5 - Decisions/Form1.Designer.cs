527da51e2c39301b9196d52c469ec9c40f4d7f8e
Carter Chapman <ChapCart405@GoTVDSB.ca>
2022-03-01 10:49:16 -0500

    Add project files.

---
+++ Part 5 - Decisions/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Part_5___Decisions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblInstuctions = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 107);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(268, 20);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // lblInstuctions
+            // 
+            this.lblInstuctions.Location = new System.Drawing.Point(9, 51);
+            this.lblInstuctions.Name = "lblInstuctions";
+            this.lblInstuctions.Size = new System.Drawing.Size(271, 30);
+            this.lblInstuctions.TabIndex = 1;
+            this.lblInstuctions.Text = "Enter Your Age";
+            this.lblInstuctions.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.Location = new System.Drawing.Point(12, 142);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(268, 106);
+            this.lblOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 257);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblInstuctions);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblInstuctions;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Part 5 - Decisions/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_5___Decisions
+{
+    public partial class Form1 : Form
+    {
+        int userAge;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            userAge = Convert.ToInt32(txtInput.Text);
+            lblOutput.Text =
+        }
+    }
+}
